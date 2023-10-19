41bffb912a180bcca4c49244c756dae5d74049f5
arilton oliveira <112397941+ariltonsilva@users.noreply.github.com>
2023-10-18 21:11:17 -0300

    Add files via upload

---
+++ InternetBrowser.java
@@ -0,0 +1,4 @@
+public interface InternetBrowser {
+    void browse(String url);
+    void download(String fileUrl);
+}
---
+++ Iphone.java
@@ -0,0 +1,47 @@
+
+public class Iphone implements MusicPlayer, Phone, InternetBrowser {
+    // Implementação dos métodos da interface MusicPlayer
+    public void play() {
+        // Implementação
+    }
+
+    public void pause() {
+        // Implementação
+    }
+
+    public void next() {
+        // Implementação
+    }
+
+    public void previous() {
+        // Implementação
+    }
+
+    // Implementação dos métodos da interface Phone
+    public void call(String phoneNumber) {
+        // Implementação
+    }
+
+    public void answer() {
+        // Implementação
+    }
+
+    public void endCall() {
+        // Implementação
+    }
+
+    // Implementação dos métodos da interface InternetBrowser
+    public void browse(String url) {
+        // Implementação
+    }
+
+    public void download(String fileUrl) {
+        // Implementação
+    }
+
+    // Método main para poder executar o programa
+    public static void main(String[] args) {
+        // Código de inicialização do programa, se necessário
+    }
+}
+
